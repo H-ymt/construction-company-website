4cb847f248f8fb336d42872cf8d142499e8cd621
H-ymt <y.handai1272@gmail.com>
2024-02-09 18:10:32 +0900

    feat(blog): styling blog detail page

--- app/_components/Article.tsx
+++ app/_components/Article.tsx
@@ -8,25 +8,27 @@ interface ArticleProps {
 
 export default function ArticleComponents({ data }: ArticleProps) {
   return (
-    <main>
-      <h1>{data.title}</h1>
+    <div className="flex flex-col gap-10">
+      <h1 className="pb-2 text-4xl font-semibold tracking-wide">{data.title}</h1>
       {data.eyecatch && (
         <Image
           src={data.eyecatch?.url}
           width={data.eyecatch?.width}
           height={data.eyecatch?.height}
-          alt="blog eyecatch"
+          alt="Blog eyecatch"
           loading="eager"
+          className="rounded-md"
         />
       )}
 
       <div>
         <div
+          className="[&_h2]:mb-2 [&_h2]:mt-6 [&_h2]:font-semibold [&_h3]:text-foreground [&_hr]:my-8 [&_li]:!list-disc [&_p]:mb-6 [&_ul]:flex [&_ul]:flex-col [&_ul]:gap-2 [&_ul]:pl-5"
           dangerouslySetInnerHTML={{
             __html: `${formatRichText(data.content)}`,
           }}
         />
       </div>
-    </main>
+    </div>
   )
 }
