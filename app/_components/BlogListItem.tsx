a32bcb4e3e8180cb81b312862244439dad6da85e
H-ymt <y.handai1272@gmail.com>
2024-02-10 01:01:38 +0900

    feat(blog list): add hover animation

--- app/_components/BlogListItem.tsx
+++ app/_components/BlogListItem.tsx
@@ -1,5 +1,6 @@
 import { Article } from '@/libs/microcms'
 import { formateDate } from '@/utils/formatDate'
+import { ArrowRightCircle } from 'lucide-react'
 import Link from 'next/link'
 
 interface BlogListItemProps {
@@ -10,17 +11,21 @@ export default async function BlogListItem({ blogs }: BlogListItemProps) {
   const formattedDate = blogs.publishedAt ? formateDate(blogs.publishedAt) : ''
 
   return (
-    <li className="pt-4">
+    <li className="group group flex justify-between border-b pb-3 pt-3 transition hover:text-primary/90">
       <Link
         href={`blog/${blogs.id}`}
-        className="item flex items-center gap-4 border-b pb-2 pl-2 text-lg"
+        className="item flex w-full items-center gap-4 pl-2 text-lg"
       >
         <span className="text-base">{formattedDate}</span>
-        <span className="rounded-sm bg-secondary-foreground px-2 py-0.5 text-xs text-white">
+        <span className="rounded-sm bg-secondary-foreground px-2 py-0.5 text-xs text-white transition group-hover:bg-primary/90">
           {blogs.category.name}
         </span>
-        <p className="font-semibold hover:text-primary">{blogs.title}</p>
+        <p className="font-semibold">{blogs.title}</p>
       </Link>
+      <ArrowRightCircle
+        strokeWidth={1.5}
+        className="-translate-x-3 transition-all group-hover:-translate-x-2 group-hover:text-primary/90"
+      />
     </li>
   )
 }
